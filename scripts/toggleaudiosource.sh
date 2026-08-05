b77ec85b2fd183c4521e1eb41aba4d9755201fd8
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2026-08-05 10:53:01 -0700

    Add shebang

--- scripts/toggleaudiosource.sh
+++ scripts/toggleaudiosource.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env bash
+#  _____                 _           _             _ _       
+# |_   _|__   __ _  __ _| | ___     / \  _   _  __| (_) ___  
+#   | |/ _ \ / _` |/ _` | |/ _ \   / _ \| | | |/ _` | |/ _ \ 
+#   | | (_) | (_| | (_| | |  __/  / ___ \ |_| | (_| | | (_) |
+#   |_|\___/ \__, |\__, |_|\___| /_/   \_\__,_|\__,_|_|\___/ 
+#            |___/ |___/                                     
+#  ____                           
+# / ___|  ___  _   _ _ __ ___ ___ 
+# \___ \ / _ \| | | | '__/ __/ _ \
+#  ___) | (_) | |_| | | | (_|  __/
+# |____/ \___/ \__,_|_|  \___\___|
+#
+# by Joshua Holmes (2026) 
+# ----------------------------------------------------- 
+                                
 # the name of this changes sometimes, so we get it programmatically
 hyprx_prefix="alsa_output.usb-HP__Inc_HyperX_Cloud_II_Wireless_0-00"
 hyprx=$(pactl list short sinks | rg ${hyprx_prefix} | cut -f2)
